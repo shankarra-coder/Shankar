3709434aaf7dbe6a8fc371eab1da939356320310
shankarra <rshankar1910@gmail.com>
2020-04-06 18:29:02 +0530

    Prime Number

--- PrimeNumber.py
+++ PrimeNumber.py
@@ -1,14 +1,14 @@
 # This Program wil identify whether a Number is Prime or Not
 
-def PrimeNumber():
+def PrimeNumber(num):
     for i in range(2,num,1):
         if num % i == 0:
             print('Number {} is NON PRIME'.format(num))
             break
         else:
             continue
-        print('Number {} is PRIME'.format(num))
+    print('Number {} is PRIME'.format(num))
 
 print('Enter an Integer Number :')
 num = int(input())
-PrimeNumber()
+PrimeNumber(num)
